c70d689ed6002d2a6ff6a8f131c1f8a91094f4f3
PeterLenon <goshomip@gmail.com>
2023-07-18 00:04:23 -0400

    shiny web app

--- tesla_Arima.R
+++ tesla_Arima.R
@@ -2,29 +2,47 @@ library(forecast)
 library(tseries)
 library(quantmod)
 library(xts)
+library(shiny)
+library(shinythemes)
 
-getSymbols('TSLA')
-head(TSLA)
-tesla_ts <- ts(TSLA)
-plot(tesla_ts)
-colnames(tesla_ts)
-colnames(tesla_ts) <- c("Open", "High", "Low", "Close", "Volume", "Adjusted")
-colnames(tesla_ts)
-plot(tesla_ts[,'Adjusted'])
-autoplot(tesla_ts[,'Adjusted'])
+ui <- fluidPage(theme = shinytheme("cerulean"),
+                navbarPage(
+                  # theme = "cerulean",  # <--- To use a theme, uncomment this
+                  "My first app",
+                  tabPanel("Navbar 1",
+                           sidebarPanel(
+                             tags$h3("Input:"),
+                             textInput("txt1", "Given Name:", ""),
+                           ), # sidebarPanel
+                           mainPanel(
+                             h3("Forecast"),
+                             plotOutput("ARIMA_forecast")
+                           )
+                           
+                  ), # Navbar 1, tabPanel
+                  
+                ) # navbarPage
+) # fluidPage
+                
+                
+               
 
-adf.test(tesla_ts[,"Adjusted"])
-tesla_ts_Ad_d1 <- diff(tesla_ts[,"Adjusted"])
-adf.test(tesla_ts_Ad_d1)
+server <- function(input, output){
+  symbol <- reactive({input$txt1})
+  getSymbols(symbol)
+  tesla_ts <- ts(symbol)
+  colnames(tesla_ts) <- c("Open", "High", "Low", "Close", "Volume", "Adjusted")
+  adf.test(tesla_ts[,"Adjusted"])
+  tesla_ts_Ad_d1 <- diff(tesla_ts[,"Adjusted"])
+  adf.test(tesla_ts_Ad_d1)
+  pacf(tesla_ts_Ad_d1)
+  Acf(tesla_ts_Ad_d1)
+  tsMod <- Arima(y=tesla_ts[,"Adjusted"], order = c(7,1,8))
+  
+  output$ARIMA_forecast <- renderplot({forecast(tsMod, h = 365)})
+}
 
-plot(tesla_ts_Ad_d1)
+shinyApp(ui = ui, server = server)
 
-Pacf(tesla_ts_Ad_d1)
-Acf(tesla_ts_Ad_d1)
 
-tsMod <- Arima(y=tesla_ts[,"Adjusted"], order = c(7,1,8))
-print(tsMod)
 
-forecast(tsMod, h= 30)
-
-autoplot(forecast(tsMod, h = 365))
