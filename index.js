0fb78fe95042231eab5f8f201cc3fc6b41f4aa3d
Peter Lenon <goshomip@gmail.com>
2023-07-24 23:51:19 -0400

    interactive stock listings

--- index.js
+++ index.js
@@ -1,31 +1,15 @@
-
-function fetchStocks()
+function elevateStock(obj)
 {
-    var elements = document.getElementsByClassName('stock');
-    for(var i = 0; i< elements.length; i++)
-    {
-        const stockSymbol = elements[i].innerHTML;
-        const yahoo = `https://query1.finance.yahoo.com/v7/finance/quote?symbols=${stockSymbol}`;
-
-        fetch(yahoo)
-        .then(response => response.json())
-        .then(data => {
-            const stockInfo = data.quoteResponse.result[0];
-
-            // Access the desired data (example: stock symbol, current price)
-            const symbol = stockInfo.symbol;
-            const price = stockInfo.regularMarketPrice;
-
-            // Display the stock information
-            alert(`Symbol: ${symbol}` `Price: ${price}` )
-            // console.log(`Symbol: ${symbol}`);
-            // console.log(`Price: ${price}`);
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-    }
-
+    obj.style.height = '60px';
 }
 
-fetchStocks();
+function de_elevateStock(obj)
+{
+    obj.style.height = '50px';
+}
+
+function clicked(obj)
+{
+    var stock = obj.innerHTML;
+    window.alert('fetching '+ stock + ' stock information from Yahoo');
+}
